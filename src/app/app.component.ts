fd9ea440fb30e5f4c0981e2ec1364069f05daca8
onkarzero <onkar.singhkaur@gmail.com>
2020-08-12 11:45:50 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-angular-crud-web-app';
+  headerTitle = 'Welcome to my first Angular CRUD web app';
+}
